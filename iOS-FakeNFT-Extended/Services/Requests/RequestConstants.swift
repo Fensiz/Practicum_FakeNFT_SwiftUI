e7daefbc0d1030f35bf8c9ec949a5918a1d99867
Симонов Иван дмитриевич <fensiz@me.com>
2025-10-08 18:50:05 +0300

    [fix] added token

--- iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
+++ iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
@@ -1,5 +1,4 @@
 enum RequestConstants {
-    static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
-#warning("insert your token here")
-    static let token = ""
+	static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
+	static let token = "1baa3df6-0b8f-4f03-9090-1fef3e07b699"
 }
