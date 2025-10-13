bfcc2c2db6c8b2b18848fe0281d196f6128bd01f
Симонов Иван дмитриевич <fensiz@me.com>
2025-10-14 00:13:52 +0300

    [feature] added Tuist
    # Conflicts:
    #	iOS-FakeNFT-Extended.xcodeproj/project.pbxproj
    #	iOS-FakeNFT-Extended/Localizable.xcstrings

---
+++ Project.swift
@@ -0,0 +1,89 @@
+import ProjectDescription
+
+let project = Project(
+	name: "iOS-FakeNFT-Extended",
+	organizationName: "com.example",
+	packages: [
+		.package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.12.0"),
+		.package(url: "https://github.com/relatedcode/ProgressHUD.git", from: "14.1.3")
+	],
+	settings: .settings(
+		base: ["SWIFT_VERSION": "6.0"],
+		configurations: [
+			.debug(name: "Debug", xcconfig: "./xcconfigs/project_iOS-FakeNFT-Extended.xcconfig"),
+			.release(name: "Release", xcconfig: "./xcconfigs/project_iOS-FakeNFT-Extended.xcconfig")
+		]
+	),
+	targets: [
+		.target(
+			name: "iOS-FakeNFT-Extended",
+			destinations: [.iPhone],
+			product: .app,
+			bundleId: "com.example.iOS-FakeNFT-Extended",
+			deploymentTargets: .iOS("17.4"),
+			infoPlist: .file(path: "iOS-FakeNFT-Extended/Info.plist"),
+			sources: ["iOS-FakeNFT-Extended/**"],
+			resources: [
+				"Resources/Assets.xcassets",
+				"Resources/**/*.storyboard",
+				"Resources/**/*.xcstrings"
+			],
+			scripts: [
+				.post(
+					path: "./scripts/swiftlint.sh",
+					name: "SwiftLint",
+					basedOnDependencyAnalysis: false
+				)
+			],
+			dependencies: [
+				.package(product: "Kingfisher"),
+				.package(product: "ProgressHUD")
+			],
+			settings: .settings(
+				base: ["SWIFT_VERSION": "6.0"],
+				configurations: [
+					.debug(name: "Debug", xcconfig: "./xcconfigs/target_iOS-FakeNFT-Extended.xcconfig"),
+					.release(name: "Release", xcconfig: "./xcconfigs/target_iOS-FakeNFT-Extended.xcconfig")
+				]
+			)
+		),
+		.target(
+			name: "iOS-FakeNFT-ExtendedTests",
+			destinations: [.iPhone],
+			product: .unitTests,
+			bundleId: "com.example.iOS-FakeNFT-ExtendedTests",
+			deploymentTargets: .iOS("17.4"),
+			infoPlist: .default,
+			sources: ["iOS-FakeNFT-ExtendedTests/**"],
+			dependencies: [
+				.target(name: "iOS-FakeNFT-Extended")
+			],
+			settings: .settings(
+				base: ["SWIFT_VERSION": "6.0"],
+				configurations: [
+					.debug(name: "Debug", xcconfig: "./xcconfigs/target_iOS-FakeNFT-ExtendedTests.xcconfig"),
+					.release(name: "Release", xcconfig: "./xcconfigs/target_iOS-FakeNFT-ExtendedTests.xcconfig")
+				]
+			)
+		),
+		.target(
+			name: "iOS-FakeNFT-ExtendedUITests",
+			destinations: [.iPhone],
+			product: .uiTests,
+			bundleId: "com.example.iOS-FakeNFT-ExtendedUITests",
+			deploymentTargets: .iOS("17.4"),
+			infoPlist: .default,
+			sources: ["iOS-FakeNFT-ExtendedUITests/**"],
+			dependencies: [
+				.target(name: "iOS-FakeNFT-Extended")
+			],
+			settings: .settings(
+				base: ["SWIFT_VERSION": "6.0"],
+				configurations: [
+					.debug(name: "Debug", xcconfig: "./xcconfigs/target_iOS-FakeNFT-ExtendedUITests.xcconfig"),
+					.release(name: "Release", xcconfig: "./xcconfigs/target_iOS-FakeNFT-ExtendedUITests.xcconfig")
+				]
+			)
+		)
+	]
+)
