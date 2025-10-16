18d1c4a123d5ca837f589326fe2757bc6be11aa7
Vladimir <bizin.vladimir01@gmail.com>
2025-10-16 11:52:24 +0300

    [feature]: add navigation bar appearance setup

--- iOS-FakeNFT-Extended/iOSFakeNFTExtendedApp.swift
+++ iOS-FakeNFT-Extended/iOSFakeNFTExtendedApp.swift
@@ -2,19 +2,25 @@ import SwiftUI
 
 @main
 struct FakeNFTExtendedApp: App {
-	init() {
-		let appearance = UITabBarAppearance()
-		appearance.configureWithDefaultBackground()
-		appearance.backgroundColor = .ypWhite
-		appearance.shadowColor = .clear
-		UITabBar.appearance().standardAppearance = appearance
-		UITabBar.appearance().scrollEdgeAppearance = appearance
-	}
+    init() {
+        let tabAppearance = UITabBarAppearance()
+        tabAppearance.configureWithDefaultBackground()
+        tabAppearance.backgroundColor = .ypWhite
+        tabAppearance.shadowColor = .clear
+        UITabBar.appearance().standardAppearance = tabAppearance
+        UITabBar.appearance().scrollEdgeAppearance = tabAppearance
 
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-				.environment(ServicesAssembly(networkClient: DefaultNetworkClient(), nftStorage: NftStorageImpl()))
-		}
-	}
+        let navAppearance = UINavigationBarAppearance()
+        navAppearance.configureWithOpaqueBackground()
+        navAppearance.backgroundColor = .ypWhite
+        UINavigationBar.appearance().standardAppearance = navAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navAppearance
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(ServicesAssembly(networkClient: DefaultNetworkClient(), nftStorage: NftStorageImpl()))
+        }
+    }
 }
