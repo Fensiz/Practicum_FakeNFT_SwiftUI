3ab4aa202cf5f5f25a003c9458aa75e74c797a5a
Vladimir <bizin.vladimir01@gmail.com>
2025-10-19 19:14:42 +0300

    [feature]: add empty view models and views

---
+++ iOS-FakeNFT-Extended/Catalog/CollectionDetailView/Components/AuthorDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  AuthorDetailView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Vladimir on 19.10.2025.
+//  Copyright © 2025 com.example. All rights reserved.
+//
+
---
+++ iOS-FakeNFT-Extended/Catalog/CollectionDetailView/NFTCollectionDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NFTCollectionDetailViewModel.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Vladimir on 19.10.2025.
+//  Copyright © 2025 com.example. All rights reserved.
+//
+
