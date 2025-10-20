2716ed277b8f7f1b52ed007d976a731ceb7eb51a
Vladimir <bizin.vladimir01@gmail.com>
2025-10-20 15:25:52 +0300

    [feature]: add new screens to Screen model

--- iOS-FakeNFT-Extended/Models/Navigation/Screen.swift
+++ iOS-FakeNFT-Extended/Models/Navigation/Screen.swift
@@ -11,28 +11,32 @@ enum Screen: Hashable {
 	case dummy
 	case payment(coordinator: any CartCoordinator, action: () -> Void)
 	case successPayment(action: () -> Void)
+	case collectionDetails(collection: NFTCollectionModel, coordinator: any CatalogCoordinatorProtocol)
 	case web(url: URL)
 
 	static func == (lhs: Screen, rhs: Screen) -> Bool {
 		switch (lhs, rhs) {
-			case (.dummy, .dummy): true
-			case (.payment, .payment): true
-			case (.web, .web): true
-			case (.successPayment, .successPayment): true
-			default: false
+		case (.dummy, .dummy): true
+		case (.payment, .payment): true
+		case (.web, .web): true
+		case (.successPayment, .successPayment): true
+		case (.collectionDetails, .collectionDetails): true
+		default: false
 		}
 	}
 
 	func hash(into hasher: inout Hasher) {
 		switch self {
-			case .dummy:
-				hasher.combine(0)
-			case .payment:
-				hasher.combine(1)
-			case .web:
-				hasher.combine(2)
-			case .successPayment:
-				hasher.combine(3)
+		case .dummy:
+			hasher.combine(0)
+		case .payment:
+			hasher.combine(1)
+		case .web:
+			hasher.combine(2)
+		case .successPayment:
+			hasher.combine(3)
+		case.collectionDetails:
+			hasher.combine(4)
 		}
 	}
 }
