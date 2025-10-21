fda2b825dfb0325c4acfa140ad918be01a39c9d3
Hajime4life <doguziagerman@gmail.com>
2025-10-21 21:21:58 +0300

    [feature] Added viewModel with service into ProfileView

--- iOS-FakeNFT-Extended/UI/Profile/ProfileView.swift
+++ iOS-FakeNFT-Extended/UI/Profile/ProfileView.swift
@@ -8,42 +8,47 @@
 import SwiftUI
 
 struct ProfileView: View {
+    let viewModel: ProfileViewModel
     var body: some View {
         VStack(alignment: .leading) {
-            ProfileInfo(
-                user:
-                    User(
-                        id: "sdsddadasf",
-                        name: "Joaquin Phoenix",
-                        avatar: URL(string: "https://rb.gy/gkfn2l") ?? nil,
-                        nfts: [],
-                        rating: "4.6",
-                        description: "Дизайнер из Казани, люблю цифровое искусство и бейглы."
-                    )
-            )
-            Text("Joaquin Phoenix.com")
-                .foregroundColor(.ypUBlue)
-                .padding(.top, 8)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            navButton(title: "Мои NFT", count: 112)
-                .padding(.top, 40)
-            navButton(title: "Избранные NFT", count: 11)
-            Spacer()
+            if let user = viewModel.user {
+                ProfileInfo(user: user)
+                
+                if let website = user.website {
+                    Text(website.absoluteString)
+                        .lineLimit(1)
+                        .foregroundColor(.ypUBlue)
+                        .padding(.top, 8)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                
+                navButton(title: "Мои NFT", count: user.nfts.count)
+                    .padding(.top, 40)
+                navButton(title: "Избранные NFT", count: user.likes?.count ?? 0)
+                Spacer()
+            } else {
+                ProgressView() // TODO: Заменить на LoadingView(), добавить стейты
+            }
         }
         .padding(.horizontal)
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
-                // TODO: Добавить переход на редактирование
-                Button(action: { print("Edit Tapped!") }) {
-                    Image(systemName: "square.and.pencil")
-                        .resizable()
-                        .frame(width: 26.5, height: 26.5)
+                if let user = viewModel.user { // TODO: Заменить на стейт
+                    // TODO: Добавить переход на редактирование
+                    Button(action: { print("Edit Tapped!") }) {
+                        Image(systemName: "square.and.pencil")
+                            .resizable()
+                            .frame(width: 26.5, height: 26.5)
+                    }
+                    .foregroundColor(.ypBlack)
                 }
-                .foregroundColor(.ypBlack)
             }
         }
         .toolbarRole(.editor)
         .background(Color.ypWhite)
+        .task {
+            await viewModel.loadProfile()
+        }
     }
     // MARK: - SubViews:
     @ViewBuilder
@@ -62,7 +67,12 @@ struct ProfileView: View {
 }
 
 #Preview {
-    NavigationView {
-        ProfileView()
+    NavigationStack {
+        ProfileView(
+            viewModel: ProfileViewModel(
+                profileService: ProfileServiceImpl(networkClient: DefaultNetworkClient()
+                )
+            )
+        )
     }
 }
