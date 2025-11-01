ce8d9613ccd60f826df03bff397597e65c62f313
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-11-01 13:00:04 +0300

    [Refactor] Remove unused fields from NFTItem

--- iOS-FakeNFT-Extended/Models/NFTItem.swift
+++ iOS-FakeNFT-Extended/Models/NFTItem.swift
@@ -7,34 +7,10 @@
 //
 import Foundation
 
-///  Данные будут подключены к /api/v1/collections/{collection_id} и /api/v1/collections
-struct NFTItem: Identifiable, Hashable, Sendable {
-    let createdAt: String?
-    let name: String
-    let cover: String?
-    let nfts: [String]
-    let description: String?
-    let author: String?
+struct NFTItem: Identifiable, Hashable, Sendable, Decodable {
     let id: String
-    let images: [URL]?
-
-    init(
-        name: String,
-        nfts: [String],
-        id: String,
-        createdAt: String? = nil,
-        cover: String? = nil,
-        description: String? = nil,
-        author: String? = nil,
-        images: [URL]? = nil
-    ) {
-        self.createdAt = createdAt
-        self.name = name
-        self.cover = cover
-        self.nfts = nfts
-        self.description = description
-        self.author = author
-        self.id = id
-        self.images = images
-    }
+    let name: String
+    let images: [URL]
+    let rating: Int
+    let price: Double
 }
