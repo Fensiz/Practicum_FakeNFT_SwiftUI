370ef7d7e0b4af4bc661f62457f3b54495c2a784
Vladimir <bizin.vladimir01@gmail.com>
2025-11-01 11:53:26 +0300

    [feature]: add first iteration of collection details service

--- iOS-FakeNFT-Extended/Catalog/Services/NFTCollectionDetailsService/NFTCollectionDetailsService.swift
+++ iOS-FakeNFT-Extended/Catalog/Services/NFTCollectionDetailsService/NFTCollectionDetailsService.swift
@@ -15,6 +15,82 @@ protocol NFTCollectionDetailsServiceProtocol: Sendable {
 	func updateFavoriteStatus(nftID: NFTModel.ID) async throws
 }
 
+actor NFTCollectionDetailsService: NFTCollectionDetailsServiceProtocol {
+
+	private let networkClient: any NetworkClient
+
+	init(networkClient: any NetworkClient = DefaultNetworkClient()) {
+		self.networkClient = networkClient
+	}
+
+	func fetchNFTs(collectionID: NFTCollectionModel.ID) async throws -> [NFTModel] {
+		let collection = try await fetchCollection(id: collectionID)
+		let nftIDs = collection.nftIDs
+		let nftNetworkModels = try await withThrowingTaskGroup(
+			of: NFTNetworkModel.self,
+			returning: [NFTNetworkModel].self
+		) { [weak self] group in
+			guard let self else {
+				return []
+			}
+			for nftID in nftIDs {
+				group.addTask {
+					let request = NFTByIDRequest(id: nftID)
+					return try await self.networkClient.send(request: request)
+				}
+			}
+			var result: [NFTNetworkModel] = []
+			for try await nft in group {
+				result.append(nft)
+			}
+			return result
+		}
+		return nftNetworkModels.map { NFTModel(networkModel: $0) }
+	}
+
+	func fetchAuthor(collectionID: NFTCollectionModel.ID) async throws -> NFTUserModel {
+		let collection = try await fetchCollection(id: collectionID)
+		let request = UserByIDRequest(id: UUID())
+		let authorNetworkModel: NFTUserNetworkModel = try await networkClient.send(request: request)
+		return NFTUserModel(networkModel: authorNetworkModel)
+	}
+
+	func updateCartStatus(nftID: NFTModel.ID) async throws {
+		let orderGetRequest = OrdersRequest(httpMethod: .get, nftIDs: nil)
+		let response: NFTOrderNetworkModel = try await networkClient.send(request: orderGetRequest)
+		var nftIDs = response.nftIDs
+		if nftIDs.contains(nftID) {
+			nftIDs.removeAll(where: { $0 == nftID })
+		} else {
+			nftIDs.append(nftID)
+		}
+		let orderPutRequest = OrdersRequest(httpMethod: .put, nftIDs: nftIDs)
+		_ = try await networkClient.send(request: orderPutRequest)
+	}
+
+	func updateFavoriteStatus(nftID: NFTModel.ID) async throws {
+		let profileGetRequest = ProfileRequest(httpMethod: .get)
+		print("fetching favourites")
+		let profile: NFTProfileNetworkModel = try await networkClient.send(request: profileGetRequest)
+		var likes = profile.likes
+		if likes.contains(nftID) {
+			likes.removeAll(where: { $0 == nftID })
+		} else {
+			likes.append(nftID)
+		}
+		let profilePutRequest = ProfileRequest(httpMethod: .put, likes: likes)
+		print("updating favourites")
+		_ = try await networkClient.send(request: profilePutRequest)
+	}
+
+	private func fetchCollection(id: NFTCollectionModel.ID) async throws -> NFTCollectionNetworkModel {
+		let request = CollectionByIDRequest(id: id)
+		let collection: NFTCollectionNetworkModel = try await networkClient.send(request: request)
+		return collection
+	}
+
+}
+
 actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 
 	private let throwsError: Bool
@@ -23,8 +99,8 @@ actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 		.init(
 			id: UUID(),
 			authorID: UUID(),
-			name: "Myrna Cervantes",
-			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Ellsa/1.png")!,
+			title: "Myrna Cervantes",
+			imageURLs: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Ellsa/1.png")!],
 			rating: 5,
 			price: 39.37,
 			currency: .eth
@@ -32,8 +108,8 @@ actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 		.init(
 			id: UUID(),
 			authorID: UUID(),
-			name: "Melvin Yang",
-			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Gray/Dominique/1.png")!,
+			title: "Melvin Yang",
+			imageURLs: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Gray/Dominique/1.png")!],
 			rating: 3,
 			price: 8.04,
 			currency: .eth
@@ -41,8 +117,8 @@ actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 		.init(
 			id: UUID(),
 			authorID: UUID(),
-			name: "Sharon Paul",
-			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Lucky/1.png")!,
+			title: "Sharon Paul",
+			imageURLs: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Lucky/1.png")!],
 			rating: 3,
 			price: 27.32,
 			currency: .eth
@@ -50,8 +126,8 @@ actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 		.init(
 			id: UUID(),
 			authorID: UUID(),
-			name: "Mattie McDaniel",
-			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Brown/Emma/1.png")!,
+			title: "Mattie McDaniel",
+			imageURLs: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Brown/Emma/1.png")!],
 			rating: 1,
 			price: 28.82,
 			currency: .eth
@@ -59,8 +135,8 @@ actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 		.init(
 			id: UUID(),
 			authorID: UUID(),
-			name: "Myrna Cervantes",
-			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Ellsa/1.png")!,
+			title: "Myrna Cervantes",
+			imageURLs: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Ellsa/1.png")!],
 			rating: 5,
 			price: 39.37,
 			currency: .eth
@@ -68,8 +144,8 @@ actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 		.init(
 			id: UUID(),
 			authorID: UUID(),
-			name: "Melvin Yang",
-			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Gray/Dominique/1.png")!,
+			title: "Melvin Yang",
+			imageURLs: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Gray/Dominique/1.png")!],
 			rating: 3,
 			price: 8.04,
 			currency: .eth
@@ -77,8 +153,8 @@ actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 		.init(
 			id: UUID(),
 			authorID: UUID(),
-			name: "Sharon Paul",
-			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Lucky/1.png")!,
+			title: "Sharon Paul",
+			imageURLs: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Lucky/1.png")!],
 			rating: 3,
 			price: 27.32,
 			currency: .eth
@@ -86,8 +162,8 @@ actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 		.init(
 			id: UUID(),
 			authorID: UUID(),
-			name: "Mattie McDaniel",
-			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Brown/Emma/1.png")!,
+			title: "Mattie McDaniel",
+			imageURLs: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Brown/Emma/1.png")!],
 			rating: 1,
 			price: 28.82,
 			currency: .eth
@@ -95,8 +171,8 @@ actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 		.init(
 			id: UUID(),
 			authorID: UUID(),
-			name: "Myrna Cervantes",
-			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Ellsa/1.png")!,
+			title: "Myrna Cervantes",
+			imageURLs: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Ellsa/1.png")!],
 			rating: 5,
 			price: 39.37,
 			currency: .eth
@@ -104,8 +180,8 @@ actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 		.init(
 			id: UUID(),
 			authorID: UUID(),
-			name: "Melvin Yang",
-			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Gray/Dominique/1.png")!,
+			title: "Melvin Yang",
+			imageURLs: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Gray/Dominique/1.png")!],
 			rating: 3,
 			price: 8.04,
 			currency: .eth
@@ -113,8 +189,8 @@ actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 		.init(
 			id: UUID(),
 			authorID: UUID(),
-			name: "Sharon Paul",
-			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Lucky/1.png")!,
+			title: "Sharon Paul",
+			imageURLs: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Beige/Lucky/1.png")!],
 			rating: 3,
 			price: 27.32,
 			currency: .eth
@@ -122,8 +198,8 @@ actor NFTCollectionDetailsMockService: NFTCollectionDetailsServiceProtocol {
 		.init(
 			id: UUID(),
 			authorID: UUID(),
-			name: "Mattie McDaniel",
-			imageURL: URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Brown/Emma/1.png")!,
+			title: "Mattie McDaniel",
+			imageURLs: [URL(string: "https://code.s3.yandex.net/Mobile/iOS/NFT/Brown/Emma/1.png")!],
 			rating: 1,
 			price: 28.82,
 			currency: .eth
