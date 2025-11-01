a7741fd786883dfb3c7abb6f8a2b6b037520ab81
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-11-01 13:03:11 +0300

    [Feature] Added parameters for pagination and server-side sorting in UsersRequest

--- iOS-FakeNFT-Extended/Services/Requests/UsersRequest.swift
+++ iOS-FakeNFT-Extended/Services/Requests/UsersRequest.swift
@@ -8,9 +8,30 @@
 import Foundation
 
 struct UsersRequest: NetworkRequest, Sendable {
+    let page: Int
+    let size: Int
+    let sortBy: String?
+
+    init(page: Int = 0, size: Int = 7, sortBy: String? = nil) {
+        self.page = page
+        self.size = size
+        self.sortBy = sortBy
+    }
+
     var endpoint: URL? {
         var comps = URLComponents(string: RequestConstants.baseURL)
-        comps?.path = "/api/v1/users"
+        comps?.path = API.Users.list
+
+       var queryItems = [
+            URLQueryItem(name: "page", value: "\(page)"),
+            URLQueryItem(name: "size", value: "\(size)")
+        ]
+
+        if let sortBy = sortBy {
+            queryItems.append(URLQueryItem(name: "sortBy", value: sortBy))
+        }
+
+        comps?.queryItems = queryItems
         return comps?.url
     }
 }
