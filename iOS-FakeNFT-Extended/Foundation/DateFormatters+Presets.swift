07159c8e11febe10bcbb67998fd6c1c33cedf403
Симонов Иван дмитриевич <fensiz@me.com>
2025-11-04 14:31:23 +0300

    [refactoring] updated project structure

--- iOS-FakeNFT-Extended/Foundation/DateFormatters+Presets.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-extension DateFormatter {
-    // We use static var because creating a dateFormatter is an expensive operation and we should do it once
-    @MainActor static let defaultDateFormatter: ISO8601DateFormatter = .init()
-}
